fda61acd7a247ffaf221d4eaff3160ba4db0c5b6
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-06-13 22:14:37 +0300

    Remove unused annotation

--- src/test/groovy/wage_controller/WageControllerTest.groovy
+++ src/test/groovy/wage_controller/WageControllerTest.groovy
@@ -3,7 +3,6 @@ package wage_controller
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
-import spock.lang.Unroll
 import wage_calculator.WageController
 
 import static org.hamcrest.core.IsEqual.equalTo
@@ -13,7 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class WageControllerSpec extends Specification {
     def mvc = MockMvcBuilders.standaloneSetup(new WageController()).build()
 
-    @Unroll
     def 'When getting the /hello endpoint, "Hello!" should be returned with OK status'() {
         expect:
             mvc.perform(MockMvcRequestBuilders.get('/hello'))
