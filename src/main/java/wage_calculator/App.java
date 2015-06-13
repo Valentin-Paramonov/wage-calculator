86a60f8a4f0e15e12d5232e585c84df3aad1ce5b
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-06-13 22:11:12 +0300

    Add main entry point

--- src/main/java/wage_calculator/App.java
+++ src/main/java/wage_calculator/App.java
@@ -1,7 +1,11 @@
 package wage_calculator;
 
-/**
- * Created by valentine-personal on 15.13.6.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
