c127cde2185365cfcbbbf8383688fd296283eb21
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-06-15 23:58:32 +0300

    Configure require.js

---
+++ src/main/resources/static/js/alert.js
@@ -0,0 +1,5 @@
+define({
+    alertHi: function() {
+        window.alert('Hi!');
+    }
+});
---
+++ src/main/resources/static/js/config.js
@@ -0,0 +1,6 @@
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min'
+    }
+});
